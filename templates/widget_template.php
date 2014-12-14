0e452802ac7353944f1e269dc052344c4fcef908
kiliar <lpfankiliar@gmail.com>
2014-12-15 01:08:02 +0200

    Small update#2

--- templates/widget_template.php
+++ templates/widget_template.php
@@ -1,18 +1,28 @@
+<!-- Да простит меня бог за такие издевательства -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <style type="text/css">
 	.Super-widget-the-best-of-the-best {
 		background: #fff;
 		color: #000!important;
 		list-style: none!important;
 		border-radius: 5px;
-		
+
 	}
 </style>
 <div class="Super-widget-the-best-of-the-best">
 	<b><?php echo $instance[ 'title' ]; ?></b>
-	<ul>
+	<ul class="container">
 		<?php $loop = new WP_Query( array( 'post_type' => 'post-type-template', '3' => -1 ) ); ?>
 		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-		<li>
+		<li class="col-md-2">
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			<span>Post Meta b:<?php echo get_post_meta(the_id(), 'meta_b', true); ?>meta_b</span>
 		</li>	
